c0d461293b9579ecdeaf781dc367d5681159e9d7
deaxparadox <nitish136900kushwaha@gmail.com>
2024-04-15 18:44:11 +0530

    test updates

---
+++ implementation/dsa-rs/src/algorithms/merge_operation.rs
@@ -0,0 +1,8 @@
+pub fn main() {
+    let mut v1 = vec![1, 2, 4, 5];
+    let mut v2 = vec![2, 9, 12, 15];
+}
+
+fn merge_operation(a: Vec<i32>, m: i32, b: Vec<i32>, n: i32, c: Vec<i32>) {
+
+}
--- implementation/dsa-rs/src/algorithms/mod.rs
+++ implementation/dsa-rs/src/algorithms/mod.rs
@@ -1,3 +1,4 @@
 pub mod linear_search;
 pub mod binary_search;
 pub mod sort;
+pub mod merge_operation;
